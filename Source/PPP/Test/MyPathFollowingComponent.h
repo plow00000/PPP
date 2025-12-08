7e2754424e10ac7936f8365a406f853a15272d40
plow00000 <icusene@gmail.com>
2025-12-08 17:51:48 +0900

    마지막 보정만 하면 됨

---
+++ Source/PPP/Test/MyPathFollowingComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Navigation/PathFollowingComponent.h"
+#include "MyPathFollowingComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PPP_API UMyPathFollowingComponent : public UPathFollowingComponent
+{
+	GENERATED_BODY()
+	
+};
